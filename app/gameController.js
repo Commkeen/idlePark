5ff7a2dff6b6da0e73dda500d291b47a2f7bcd48
Commkeen <commkeen4000@gmail.com>
2017-09-01 23:33:50 -0700

    Display from stat model directly

--- app/gameController.js
+++ app/gameController.js
@@ -4,26 +4,7 @@ angular.module('gameApp').
 controller('GameController',
   function($scope, $interval, buildingService, statService, techService) {
 
-    
-
-    $scope.updateValues = function() {
-      $scope.baseVisitorRateCap = statService.statModel.baseVisitorRateCap;
-    
-      $scope.ticksPerSecond = statService.statModel.ticksPerSecond;
-      $scope.ticksPerGameHour = statService.statModel.ticksPerGameHour;
-      $scope.ticksTotal = statService.statModel.ticksTotal;
-
-      $scope.money = statService.statModel.money;
-      $scope.visitorsTotal = statService.statModel.visitorsTotal;
-      $scope.incomePerVisitor = statService.statModel.incomePerVisitor;
-      $scope.visitorRateCap = statService.statModel.visitorRateCap;
-      $scope.baseVisitorRate = statService.statModel.baseVisitorRate;
-      $scope.totalTerritory = statService.statModel.totalTerritory;
-      $scope.usedTerritory = statService.statModel.usedTerritory;
-      $scope.influence = statService.statModel.influence;
-      $scope.influenceRate = statService.statModel.influenceRate;
-    };
-    $scope.updateValues();
+    $scope.stats = statService.statModel;
 
     $scope.getDateTime = function() {
       let currentDateInMs = statService.statModel.startDate.valueOf();
@@ -37,7 +18,6 @@ controller('GameController',
     $scope.tick = function() {
       statService.statModel.ticksTotal = statService.statModel.ticksTotal + 1;
       statService.recalculateStats();
-      $scope.updateValues();
     };
 
     $scope.resetGame = function() {
